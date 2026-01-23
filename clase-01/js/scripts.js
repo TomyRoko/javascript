eb4c5b351bcc52de04cb44899dea3dce6f7ac725
tomyroko <tomyroko@gmail.com>
2026-01-23 21:36:04 +0100

    erg

--- clase-01/js/scripts.js
+++ clase-01/js/scripts.js
@@ -1,2 +1,14 @@
 console.log("Tzvetomir Vesselinov Dochev");
 console.log("25 Años");
+
+let nombre = "Tzvetomir";
+let apellido = "vesselinov Dochev";
+
+console.log("Mi nombre es: " + nombre + " " + apellido);
+console.log(`Mi nombre es: ${nombre} ${apellido}`);
+
+
+let texto = "Bienvenidos a la clase de JavaScript";
+let numero = 2024;
+let undefined; 
+

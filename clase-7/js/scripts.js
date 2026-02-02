f28d95db5a4434f6b2e198a9f305ba43f6c70780
tomyroko <tomyroko@gmail.com>
2026-02-02 20:27:07 +0100

    jjk

--- clase-7/js/scripts.js
+++ clase-7/js/scripts.js
@@ -1,6 +1,35 @@
-const titulo = document.getElementById("titulo") 
+const titulo = document.getElementById("titulo");
 
-console.log(titulo.textContent) 
+console.log(titulo.textContent);
+
+titulo.textContent = "Pagina de motos";
+
+const subtitulo = document.getElementById("subtitulo");
+
+console.log(subtitulo.textContent);
+
+subtitulo.textContent = "Subtitulo de motos";
+
+const descripciones = document.getElementsByClassName("descripcion");
+
+console.log(descripciones.length);
+
+descripciones[0].textContent = "Descripcion de autos";
+descripciones[1].textContent = "Descripcion de motos";
+
+console.log(descripciones[0].textContent);
+console.log(descripciones[1].textContent);
+
+for (let i = 0; i < descripciones.length; i++) {
+console.log(descripciones[i]);    
+}
+
+descripciones[0].style.color = "red";
+descripciones[0].style.fontSize = "20px";
+descripciones[1].style.color = "blue";
+descripciones[1].style.fontSize = "30px";
+
+descripciones[0].style.backgroundColor = "yellow";
+descripciones[1].style.backgroundColor = "lightgrey";
 
 
-titulo.textContent = "Pagina de motos"
